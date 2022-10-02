d782e5996f6cb213af1f9fe03d0fe26d58ac00f6
yeonfish6040 <yeonfish6040@gmail.com>
2022-10-02 15:29:22 +0900

    update

--- boardPrac/boardPrac/src/main/java/com/board/boardprac/mapper/TimeMapper.java
+++ boardPrac/boardPrac/src/main/java/com/board/boardprac/mapper/TimeMapper.java
@@ -1,11 +1,13 @@
 package com.board.boardprac.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Service;
 
 @Service // Spring Container에 Bean 등록
 @Mapper // MyBatis 연결 등록
 public interface TimeMapper {
+    @Select("select sysdate from dual")
     public String getTime();
 
 }
---
+++ boardPrac/boardPrac/src/test/java/com/board/boardprac/mapper/TimeMapperTest.java
@@ -0,0 +1,21 @@
+package com.board.boardprac.mapper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class TimeMapperTest {
+    @Autowired
+    private TimeMapper timeMapper;
+
+    @Test
+    public void testGetTime() {
+        String sysdate = timeMapper.getTime();
+        log.info("----------------------------------");
+        log.info("Sysdate: "+sysdate);
+        log.info("----------------------------------");
+    }
+}
